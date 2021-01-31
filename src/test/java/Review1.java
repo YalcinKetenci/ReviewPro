7839c4cd9ccb6b3bb699d987abefc3100412e978
YalcinKetenci <yalcinketencischool@gmail.com>
2021-01-31 20:19:04 +0200

    Added some code

--- src/test/java/Review1.java
+++ src/test/java/Review1.java
@@ -3,6 +3,6 @@ public class Review1 {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
-        System.out.println("Some code here");
+        System.out.println("Some code  here");
     }
 }
